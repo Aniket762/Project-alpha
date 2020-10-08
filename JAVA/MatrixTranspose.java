0b56381d0eb5dbca0efc2e9be242186dc7fa81cc
Ganesh Tarone <62292892+GTron-1729@users.noreply.github.com>
2020-10-08 22:50:02 +0530

    Added Matrix Transpose code in Java

---
+++ JAVA/MatrixTranspose.java
@@ -0,0 +1,35 @@
+class MatrixTranspose 
+{ 
+    static final int N = 4; 
+      
+    // This function stores transpose 
+    // of A[][] in B[][] 
+    static void transpose(int A[][], int B[][]) 
+    { 
+        int i, j; 
+        for (i = 0; i < N; i++) 
+            for (j = 0; j < N; j++) 
+                B[i][j] = A[j][i]; 
+    } 
+      
+    // Driver code 
+    public static void main (String[] args) 
+    { 
+        int A[][] = { {1, 1, 1, 1}, 
+                      {2, 2, 2, 2}, 
+                      {3, 3, 3, 3}, 
+                      {4, 4, 4, 4}}; 
+      
+        int B[][] = new int[N][N], i, j; 
+      
+        transpose(A, B); 
+      
+        System.out.print("Result matrix is \n"); 
+        for (i = 0; i < N; i++) 
+        { 
+            for (j = 0; j < N; j++) 
+            System.out.print(B[i][j] + " "); 
+            System.out.print("\n"); 
+        } 
+    } 
+} 

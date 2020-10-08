6fdf5f24d617295895b57a9d1e2d28572c75e221
EANimesha <nimeshadilini999@gmail.com>
2020-10-08 23:06:32 +0530

    java program for adding 2 integers

---
+++ JAVA/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+public class AddTwoIntegers
+{
+	public static void main(String[] args) {
+		int first = 10;
+        int second = 20;
+
+        System.out.println("Two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+	}
+}

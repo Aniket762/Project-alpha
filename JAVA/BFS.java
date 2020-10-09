0f8e1ee187e4de8c218dda6bb203d479c634d94b
= <ranchoparikh@gmail.com>
2020-10-09 11:22:14 +0530

    Added Java implementation of BFS

--- JAVA/BFS.java
+++ JAVA/BFS.java
@@ -1,79 +1,69 @@
-import java.util.*;
-import java.io.*;
+import java.io.*; 
+import java.util.*; 
 
-//In this code, the vertex index starts from 0. The first vertex will have index 0, not 1.
-class Graph{
-    public int noOfVertices;
-    public LinkedList<Integer> adjLst[];
+class BFS
+{   
 
-    //This constructor will form the skeleton of the Graph
-    Graph(int noOfVertices){
-        
-        //Creates a graph with the given number of vertices
-        this.noOfVertices = noOfVertices;
-        adjLst = new LinkedList[noOfVertices];
+	private int noOfVertices;  
+	private LinkedList<Integer> adjLst[];  
 
-        //Adjacency lists are created for each vertex
-        for(int i=0; i<noOfVertices; i++){
-            adjLst[i] = new LinkedList();
-        }
-    }
+    //This constructor will automatically create a graph with noOfVertices vertices
+    //It will also make noOfVertices adjacency lists corresponding to each vertex
+    //An adjaceny list corresponding to vertex u will have the vertices which are neighbours of u
+    BFS(int noOfVertices) { 
+		this.noOfVertices = noOfVertices; 
+		adjLst = new LinkedList[noOfVertices + 1]; 
+		for (int i=1; i <= noOfVertices; i++) 
+			adjLst[i] = new LinkedList(); 
+	} 
 
-    //This method adds an edge between vertex u and v
-    public void addEdge(int u, int v){
-        //Represents an edge u -> v
-        adj[u].add(v);
-        
-        //Depending upon the use case (directed or undirected graph) the below piece of code can be uncommented
-        //adj[v].add(u);
-    }
-}
+    //Function to add an edge into the graph
+    //Depending upon your use case, i.e directed or undirected graph, comment/uncomment the second line
+    //I have considered an undirected graph here
+    void addEdge(int u, int v) { 
+        adjLst[u].add(v);
+        adjLst[v].add(u); 
+	} 
 
+	//prints BFS traversal from a given source vertex -> start 
+	void bfs(int start) 
+	{ 
+		//Mark all the vertices as not visited, i.e false 
+		boolean visited[] = new boolean[noOfVertices + 1]; 
+ 
+		LinkedList<Integer> queue = new LinkedList<Integer>(); 
 
-public class BFS {
-  
-  //Code for BFS that starts from vertex s
-  void bfs(int s, int noOfVertices) {
+		//We mark the source node as visited and add it inside the queue 
+		visited[start]=true; 
+		queue.add(start); 
 
-    //Marks vertices which are visited during our traversal
-    boolean[] visited = new boolean[noOfVertices];
+		while (queue.size() != 0) 
+		{ 
+			//We will dequeue this vertex and start traversing through its neighbours
+			int next = queue.poll(); 
+			System.out.print(next + " -> "); 
 
-    //This queue will take the neighbours of the head element and carry on with the traversal
-    LinkedList<Integer> visVertices = new LinkedList();
+            //Now we are traversing through each neighbour of next
+            //If it is not marked visited, we will add it in the queue
+			for(int n : adjLst[next]) { 
+				if (!visited[n]) { 
+					visited[n] = true; 
+					queue.add(n); 
+				} 
+			} 
+		} 
+	} 
 
-    //Marks the starting vertex as visited
-    visited[s] = true;
-    visVertices.add(s);
+	public static void main(String args[]) 
+	{ 
+		BFS b = new BFS(5); 
 
-    //When the queue becomes empty, it means that we have considered all the neighbours and traveresed through all the vertices in a cycle
-    while (visVertices.size() != 0) {
+		b.addEdge(1, 2); 
+		b.addEdge(2, 4); 
+		b.addEdge(4, 1); 
+		b.addEdge(3, 5); 
+		b.addEdge(2, 3);  
 
-      //We remove this from the queue since we will be taking its neighbours now
-      s = visVertices.poll();
-
-      //Prints the order of traversal
-      System.out.print(s + " ");
-      
-      //Since the adjaceny lists contains all the vertices that share an edge with s, we will loop through them and add them to our queue if they aren't visited
-      for(int n : adj[s]) {
-        if (!visited[n]) {
-          visited[n] = true;
-          visVertices.add(n);
-        }
-      }
-    }
-  }
-
-  public static void main(String args[]) {
-    int noOfVertices = 5;
-    Graph g = new Graph(noOfVertices);
-    g.addEdge(0, 1);
-    g.addEdge(1, 3);
-    g.addEdge(3, 0);
-    g.addEdge(1, 2);
-    g.addEdge(2, 4);
-
-    //Will call this function so the path can be printed for BFS
-    g.BFS(4, noOfVertices);
-  }
-}
+		b.bfs(3); 
+	} 
+} 

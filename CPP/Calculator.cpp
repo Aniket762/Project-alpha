0e5daf0154fb1347a848cc99299c5d9c1cb227ba
Mariano Echavarria <41266443+mhechavarria@users.noreply.github.com>
2020-10-10 09:35:46 -0300

    Create simple calculator cpp

---
+++ CPP/Calculator.cpp
@@ -0,0 +1,19 @@
+#include "Calculator.hpp"
+#include <string>
+
+using namespace std;
+
+string Calculator::sub(string a, string b)
+{
+    return "Please, enter a number";
+}
+
+string Calculator::mult(string a, string b)
+{
+    return "Please, enter a number";
+}
+
+string Calculator::div(string a, string b)
+{
+    return "Please, enter a number";
+}

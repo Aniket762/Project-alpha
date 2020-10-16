420a2397369c39c31c839e6094fa8cef68775054
Avi Parmar <aviparmar326@gmail.com>
2020-10-17 02:22:38 +0530

    Update Factorial.cpp

--- CPP/Factorial.cpp
+++ CPP/Factorial.cpp
@@ -1,14 +1,25 @@
-
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int num;
-    int fact = 1;
-    cout<<"Enter a number:";
-    cin>>num;
-    for(int i =1;i<=num;i++){
-        fact = fact * i;
-    }
-    cout<<"Factorial of"<<" "<<num<<" is:"<<fact;
 
+unsigned long long factorial(int number) {
+    if(number == 0) {
+        return 1;
+    }
+    return number * factorial(number - 1);
+}
+
+int main() {
+    int number;
+    cout << "Enter a number between the range of 0 to 20:-" << endl;
+    cin >> number;
+	
+    if (number < 0) {
+        cout << "Invalid Input. The entered number cannot be negative." << endl;
+    } else if (number > 20) {
+        cout << "Invalid Input. The entered number cannot exceed 20. As 20! is lasrgest factorial result that \"unsigned long long\" data type can store." << endl;
+    } else {
+        cout << factorial(number) << endl;
+    }
+
+    return 0;
 }
